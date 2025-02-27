dead96c3b4668bd1f63a39a8d14dfd582361a603
Tilda Leonartsson <tilda.leonartsson@gmail.com>
2025-02-27 19:45:06 +0100

    textimggrid

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import HeaderNav from "../components/header/header";
+import TextImgGrid from "../components/textimggrid/textimggrid";
 import Footer from "../components/footer/footer";
 
 const AboutPage = () => {
   return (
     <>
     <HeaderNav />
+    <TextImgGrid />
+    <TextImgGrid />
     <Footer />
     </>
   );
